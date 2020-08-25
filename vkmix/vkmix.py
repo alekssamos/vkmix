987f58361a364049a57507b09506c81a97f8beb3
alekssamos <aleks-samos@yandex.ru>
2020-08-25 09:58:20 +0300

    set settings, code refactoring

--- vkmix/vkmix.py
+++ vkmix/vkmix.py
@@ -1,52 +1,51 @@
 """
-Библиотека для сервиса
+| Библиотека для сервиса
 https://vkmix.com/settings/api
 """
 
-import requests, urllib.parse, json
+import json
+import urllib.parse
+
+import requests
 
 
 class VkMixException(Exception):
-	"""[summary]
-
-	Args:
-		Exception ([type]): [description]
-	"""
+	"Base Exception"
 	pass
 
 
 class VkMixApiError(VkMixException):
-	"""[summary]
-
-	Args:
-		VkMixException ([type]): [description]
-	"""
+	"Api Exception"
 	pass
 
 
-class VkMix:
+class VkMix():
 	"""API для ботов API VKMix
-Мы предоставляем открытый для всех разработчиков доступ к созданию заданий в нашей системе.
 
-Взаимодействие с API
-Всем методам необходимо передавать токен авторизации параметром api_token}.
-Успешный результат в виде
-{response: результат работы метода}
-Ошибки:
-{error: тип ошибки}
-Список доступных методов"""
+| Мы предоставляем открытый для всех разработчиков доступ к созданию заданий в
+нашей системе.
 
-	s = requests.session()
-	ua = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"
-	s.headers = {"User-Agent": ua, "X-Requested-With": "XMLHttpRequest"}
-	api_token = ""
-	url = "https://vkmix.com/api/2/"
+| Взаимодействие с API
+| Всем методам необходимо передавать токен авторизации параметром `api_token`.
 
-	def __init__(self, api_token):
-		"Авторизация"
+:raises VkMixApiError: Тип ошибки
+"""
+
+	s: any = requests.session()
+	ua: str = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"
+	s.headers: dict = {"User-Agent": ua, "X-Requested-With": "XMLHttpRequest"}
+	api_token: str = ""
+	url: str = "https://vkmix.com/api/2/"
+
+	def __init__(self, api_token: str) -> None:
+		"""Авторизация
+
+		:param api_token: Ваш ключ API
+		:type api_token: str
+		"""
 		self.api_token = api_token
 
-	def _request(self, uri, method="get", data={}, headers={}, **kw):
+	def _request(self, uri: str, method: str = "get", data: dict = {}, headers: dict = {}, **kw) -> dict:
 		method = method.lower()
 		if method not in ["get", "post"]:
 			raise ValueError("method GET or POST")
@@ -62,12 +61,9 @@ class VkMix:
 		if method == "post":
 			hndlr = self.s.post
 
-		try:
-			resp = hndlr(
-				self.url + uri, data=data, headers=headers, allow_redirects=False
-			).content.decode("utf8")
-		except:
-			raise
+		resp = hndlr(
+			self.url + uri, data=data, headers=headers, allow_redirects=False
+		).content.decode("utf8")
 
 		if resp == "":
 			raise VkMixApiError("empty response")
@@ -77,89 +73,99 @@ class VkMix:
 		except json.JSONDecodeError:
 			raise VkMixApiError("not json" + resp)
 
-		if "error" in j and not "response" in j:
+		if "error" in j and "response" not in j:
 			raise VkMixApiError(j["error"])
 
 		return j["response"]
 
-	def getServices(self):
-		"""getServices
+	def getServices(self) -> list:
+		"""Получение списка сервисов
 
-Получение списка сервисов
-Результат
-Метод возвращает список сервисов для создания заданий"""
-		
+.. seealso:: :func:`VkMix.createTask`
+
+:return: Метод возвращает список сервисов для создания заданий
+:rtype: list
+"""
+
 		return self._request("getServices")
-	
-	def createTask(self, **kw):
+
+	def createTask(self, **kw) -> dict:
 		"""Добавление нового задания
 
-Параметры
-network
-Социальная сеть задания. Укажите одно из значений:
-vk - ВКонтакте
-instagram - Инстаграм
-youtube - Ютуб
-telegram - Телеграм
-ok - Одноклассники
-twitter - Твиттер
+:param network: Социальная сеть задания. Укажите одно из значений:
 
-section
-Тип задания. Для каждой социальной сети доступны свои типы:
-vk: likes, reposts, comments, friends, groups, polls
-instagram: likes, subscribers, comments, comments_likes
-youtube: likes, friends, dislikes, comments
-twitter: retweets, followers, favorites
-ok: likes, friends, groups
-telegram: subscribers
-Для Instagram дополнительно доступны: likes_q4, subscribers_q4, likes_q5, subscribers_q5, likes_q7, subscribers_q7. 
+* vk - ВКонтакте
+* instagram - Инстаграм
+* youtube - Ютуб
+* telegram - Телеграм
+* ok - Одноклассники
+* twitter - Твиттер
 
-link
-Ссылка на объект задания.
+:type network: str
 
-count
-Количество необходимых выполнений.
+:param section: Тип задания. Для каждой социальной сети доступны свои типы:
 
-amount
-Вознаграждение пользователю за выполнение задания.
+* vk: likes, reposts, comments, friends, groups, polls
+* instagram: likes, subscribers, comments, comments_likes
+* youtube: likes, friends, dislikes, comments
+* twitter: retweets, followers, favorites
+* ok: likes, friends, groups
+* telegram: subscribers
+* Для Instagram дополнительно доступны: likes_q4, subscribers_q4, likes_q5, subscribers_q5, likes_q7, subscribers_q7.
 
-comments (опц. для section = comments)
+:type section: str
+
+:param link: Ссылка на объект задания.
+:type link: str
+
+:param count: Количество необходимых выполнений.
+:type count: int
+
+:param amount: Вознаграждение пользователю за выполнение задания.
+:type amount: int
+
+:param comments: для ``section = comments`` \
 Массив вариантов комментариев
-	# todo: массив comments может передаваться не корректно
+:type comments: list, optional
 
-poll (опц. для section = polls)
+:param poll: для ``section = polls`` \
 Номер варианта за который необходимо проголосовать
+:type poll: int, optional
 
-hourly_limit
-Лимит выполнений в час
+:param hourly_limit: Лимит выполнений в час
+:type hourly_limit: int
 
-Результат
-Метод возвращает ID созданного задания."""
+:return: Метод возвращает ID созданного задания.
+:rtype: dict
+"""
 
 		return self._request("createTask", method="post", data=kw)
 
-	def getTasks(self, ids="", count=100, offset=0):
+	def getTasks(self, ids: list = [], count: int = 100, offset: int = 0) -> list:
 		"""Получение списка заданий
 
-Параметры
-ids
-Id заданий. Если не передан - вернёт все задания
+		:param ids: Id заданий. Если не передан - вернёт все задания, defaults to []
+		:type ids: list, optional
 
-count
-Количество заданий, которые необходимо вернуть. Не более 100
+		:param count: Количество заданий, которые необходимо вернуть. Не более 100, defaults to 100
+		:type count: int, optional
 
-offset
-Смещение необходимое для выборки определенного подмножества
+		:param offset: Смещение необходимое для выборки определенного подмножества, defaults to 0
+		:type offset: int, optional
 
-Результат
-Метод возвращает список заданий."""
+		:return: Метод возвращает список заданий
+		:rtype: list
+		"""
 
-		return self._request("getTasks", ids=ids, count=count, offset=offset)
+		return self._request("getTasks",
+			ids=",".join(list(map(str, ids))),
+			count=count, offset=offset)
 
-	def getBalance(self):
+	def getBalance(self) -> float:
 		"""Получение текущего баланса аккаунта
 
-Результат
-Метод возвращает баланс аккаунта."""
+:return: Метод возвращает баланс аккаунта
+:rtype: float
+"""
 
 		return self._request("getBalance")
